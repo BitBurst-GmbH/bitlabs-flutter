95d813cd445e5bc90e2a44bfaf385d4b978b6573
Omar Raad <omaraad@Omars-MacBook-Air.local>
2022-05-13 11:44:07 +0200

    Init BitLabs Flutter Package with Example Flutter Application

---
+++ example/android/app/src/main/kotlin/ai/bitburst/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package ai.bitburst.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(title: 'BitLabs Example'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: const Center(child: Text("Hallo, BitLabs!")),
+    );
+  }
+}

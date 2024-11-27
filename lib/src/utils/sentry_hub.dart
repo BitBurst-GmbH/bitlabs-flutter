29aa1861eb6553c1aefbdaaa35574db45a5d5e69
Omar <omar.raad@prodege.com>
2024-11-27 15:57:25 +0100

    run SentryHub catch exceptions only in non-test env

--- lib/src/utils/sentry_hub.dart
+++ lib/src/utils/sentry_hub.dart
@@ -1,17 +1,22 @@
+import 'dart:io';
+
 import 'package:bitlabs/secrets.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 class SentryHub {
-  static final Hub _hub = Hub(SentryOptions(dsn: sentryDSN));
+  static final isEnvTesting = Platform.environment.containsKey('FLUTTER_TEST');
+  static final Hub? _hub = Platform.environment.containsKey('FLUTTER_TEST')
+      ? null
+      : Hub(SentryOptions(dsn: sentryDSN));
 
   static void init(String token, String uid) {
-    _hub.configureScope((scope) {
+    _hub?.configureScope((scope) {
       scope.setUser(SentryUser(id: uid));
       scope.setTag('token', token);
     });
   }
 
-  static Future<SentryId> captureException(dynamic exception) {
-    return _hub.captureException(exception);
+  static void captureException(dynamic exception) {
+    _hub?.captureException(exception);
   }
 }
